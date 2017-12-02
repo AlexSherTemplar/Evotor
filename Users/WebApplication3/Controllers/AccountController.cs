4d20bcef44cd95e93e1cddab77e2e15f2a1e049e
admin-ПК\admin <tduk-tik@mail.ru>
2017-12-02 15:01:56 +0300

    Fst Upd

---
+++ Users/WebApplication3/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Users.Infrastructure;
+using Users.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System.Web
+
+namespace Users.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel details, string returnUrl)
+        {
+            AppUser user = await UserManager.FindAsync(details.Name, details.Password);
+
+
+            return View(details);
+        }
+    }
+}
---
+++ Users/WebApplication3/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Users.Models
+{
+    public class CreateUserViewModel
+    {
+        // ...
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
